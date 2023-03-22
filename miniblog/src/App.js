043754827560cd3e35cfd8e4dab9dc5b8cf026c1
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-22 22:48:05 +0530

    twocompo

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome";
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{title}</h1>
+        <Home></Home>
       </div>
     </div>
   );
