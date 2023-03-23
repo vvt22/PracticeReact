71c0c0bc62609cbec22efb969c306b549b4b6eb3
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-23 08:28:47 +0530

    reusingprops

--- miniblog/src/Home.js
+++ miniblog/src/Home.js
@@ -21,6 +21,10 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's Blogs"
+      />
     </div>
   );
 };
